ee5f0b5e5365bc43dbdd4c54d638f2833430a9ac
Hikari Kawai <121547505+hikarikawai@users.noreply.github.com>
2023-03-14 19:23:44 +0700

    Update config.py

--- quiz/config.py
+++ quiz/config.py
@@ -1,6 +1,6 @@
 class Config:
     """Bot setup config implementation."""
 
-    token: str = ""  # should be API token provided by @botfather
+    token: str = "6164879741:AAG-RUct1m3nxyW6NoMjA6Y98NaoT8ldV80"  # should be API token provided by @botfather
     db_name: str = "melody.db"
     shelve_name: str = "shelve"
